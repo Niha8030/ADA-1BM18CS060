6c38cfab874f15cea1d0638ab2cc1bd7baabc2c6
Niha8030 <53756642+Niha8030@users.noreply.github.com>
2020-02-18 13:03:01 +0530

    Create binary_rec.cpp

---
+++ binary_rec.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+int binary(int a[],int f,int l,int key){
+   int mid = (f+l)/2;
+   if(f>l)
+       return -1;
+   else if(a[mid]==key)
+       return 1;
+   else if(key>a[mid])
+       return binary(a,mid+1,l,key);
+   else
+       return binary(a,f,mid-1,key);
+}
+
+
+int main(){
+
+    int key,n;
+    cout<<"enter the no. of elements  and key"<<endl;
+    cin>>n>>key;
+    int arr[n];
+    cout<<"enter sorted the elements in array"<<endl;
+    for(int i=0;i<n;i++){
+    
+    cin>>arr[i];
+    }
+    auto start = high_resolution_clock::now();
+    cout<<binary(arr,0,n,key)<<endl;
+    auto stop = high_resolution_clock::now();  
+    auto duration = duration_cast<microseconds>(stop-start);
+    cout<<"Timr taken :"<<duration.count()<<"microseconds"<<endl;
+    return 0;
+}
